9a13e661639aaaa0e39d0acee99cf84a41c8263c
Kcastro04 <kharellynjoycecastro@gmail.com>
2026-05-29 23:04:36 +0800

    Program.cs
    Removed commented header from Program.cs

---
+++ Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Windows.Forms;
+
+namespace UserInstallationGuide
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
